d670d0e42e0cc81d535c7e971e9234a8ea669e37
AndreasVe7 <s150501@ap.be>
2024-11-23 15:56:05 +0100

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,4 @@
+// rename this file to secrets.js
+// and enter your public access token
+// from your account on https://account.mapbox.com
+const mapboxAccessToken = 'pk.eyJ1IjoiczE1MDUwMSIsImEiOiJjbTNya2pzMXMwNjM0MnFxdmJveG9pNWJiIn0.vKTUSska6QJKYuZtxnQ75w';
